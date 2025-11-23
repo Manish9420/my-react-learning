7f90dea3381d8119e9d18c6ef86465d624a2b12f
Manish9420 <144055148+Manish9420@users.noreply.github.com>
2025-11-23 13:14:04 +0530

    Added useEffect examples

---
+++ 03-useEffect-hook/03-useEffect-hook/src/App.jsx
@@ -0,0 +1,18 @@
+// import First from "./First"
+// import Timer from "./Timer"
+// import WindowSizeTracker from "./WindowSizeTracker"
+import Users from "./Users"
+function App() {
+ 
+
+  return (
+    <>
+      {/* <First /> */}
+      {/* <Timer /> */}
+      {/* <WindowSizeTracker /> */}
+      <Users />
+    </>
+  )
+}
+
+export default App
---
+++ 03-useEffect-hook/03-useEffect-hook/src/Timer.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react"
+
+function Timer() {
+    const [seconds, setSeconds] = useState(0)
+    
+
+    useEffect(() =>{
+       const interval = setInterval(() => {
+            setSeconds((prev) => prev + 1) 
+        }, 2000)
+
+        return () =>{
+            clearInterval(interval)
+            console.log("Timer Cleard.")
+        }
+       }, [])
+
+       
+
+  return (
+    <div>
+        <h2>Seconds : {seconds}</h2>
+    </div>
+  )
+}
+
+export default Timer
